b571f107058962af06add55592f44ec835eeab6a
veethree <69903178+veethree@users.noreply.github.com>
2021-09-03 08:05:31 +0000

    Add files via upload

---
+++ script_load.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Betra_RSF
+// @version      1.3
+// @description  Betra RSF
+// @author       veethreedev
+// @match        https://rsf.is/markadir/limmidaprentun*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @require      file:///home/pawel/Documents/javascript/Betra_rsf/betra_rsf.js
+// @downloadURL  https://raw.githubusercontent.com/veethree/Betra_rsf/master/betra_rsf.js
+// ==/UserScript==
